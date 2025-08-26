3a6bf9217d8b9d478370678b4372ce7356ef25cf
mohammad_hasan <mryazdani98@outlook.com>
2025-08-26 22:16:36 +0330

    adding singleton smaple

---
+++ Singleton/Sample/SingletonExample/SingletonExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using SingletonExample;
+
+var a = Singleton.Instance;
+var b = Singleton.Instance;
+
+Console.BackgroundColor = ConsoleColor.Gray;
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine($"a: {a.Connection}" +
+                  $"b: {b.Connection}");
+b.Connection = "444";
+Console.WriteLine($"a: {a.Connection}" +
+                  $"b: {b.Connection}");
---
+++ Singleton/Sample/SingletonExample/SingletonExample/Singleton.cs
@@ -0,0 +1,11 @@
+﻿namespace SingletonExample
+{
+    public sealed class Singleton
+    {
+        private static readonly Singleton instance = new Singleton();
+
+        public static Singleton Instance => instance;
+
+        public string Connection { get; set; } = "124";
+    }
+}
