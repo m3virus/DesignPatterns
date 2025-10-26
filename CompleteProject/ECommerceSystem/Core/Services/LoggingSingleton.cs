e99581f15de33290e40050847aa07fbe34d1e6bb
MH.Yazdani <mohaymen@YAZDANI-PC>
2025-10-26 16:30:38 +0330

    adding services and complete logging singleton

---
+++ CompleteProject/ECommerceSystem/Core/Services/LoggingSingleton.cs
@@ -0,0 +1,21 @@
+﻿using Core.Models;
+
+namespace Core.Services
+{
+    public sealed class LoggingSingleton
+    {
+        private static readonly Lazy<LoggingSingleton> _instance = new Lazy<LoggingSingleton>(() => new LoggingSingleton());
+        public static LoggingSingleton Instance => _instance.Value;
+
+        private LoggingSingleton()
+        {
+            
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"[{DateTime.Now}]:{message}");
+        }
+
+    }
+}
